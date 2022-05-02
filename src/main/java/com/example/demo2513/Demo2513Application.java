14ca9598cb3251bb9685eeff1480977f60bd5683
tianh <tianhlalop1a@gmail.com>
2022-05-02 12:52:49 +0630

    make it better

---
+++ src/main/java/com/example/demo2513/Demo2513Application.java
@@ -0,0 +1,13 @@
+package com.example.demo2513;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2513Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2513Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo2513/Demo2513ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo2513;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2513ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
