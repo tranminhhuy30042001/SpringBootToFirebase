0395e1ed137d19741916f9d5f9cac1225105d02f
tianh <tianhlalop1a@gmail.com>
2022-05-26 13:34:59 +0630

    asd

--- src/main/java/com/example/demo2513/entity/Product.java
+++ src/main/java/com/example/demo2513/entity/Product.java
@@ -3,7 +3,34 @@ package com.example.demo2513.entity;
 
 public class Product {
     private String name;
+    private String price;
+    private String type;
     private String description;
+    private String imageView;
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getImageView() {
+        return imageView;
+    }
+
+    public void setImageView(String imageView) {
+        this.imageView = imageView;
+    }
 
     public String getName() {
         return name;
