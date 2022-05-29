2789256e4faeb59b5d39ea2e2bae1ecb1e62abb4
tianh <tianhlalop1a@gmail.com>
2022-05-29 08:25:50 +0630

    add email to cart

--- src/main/java/com/example/demo2513/entity/Cart.java
+++ src/main/java/com/example/demo2513/entity/Cart.java
@@ -3,11 +3,20 @@ package com.example.demo2513.entity;
 import java.util.List;
 
 public class Cart {
+    private String email;
     private String name;
     private String address;
     private List<Product> listorder;
     private String orderdate;
     private String phone;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getName() {
         return name;
