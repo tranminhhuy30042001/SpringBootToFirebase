6b3ac02cabe22c61ebbefc80520c5f755b312930
tianh <tianhlalop1a@gmail.com>
2022-06-13 20:23:42 +0630

    asd

--- src/main/java/com/example/demo2513/entity/User.java
+++ src/main/java/com/example/demo2513/entity/User.java
@@ -6,7 +6,10 @@ public class User {
 
     private String username;
     private String password;
+
     private String email;
+
+    private String phone;
 
     public String getEmail() {
         return email;
